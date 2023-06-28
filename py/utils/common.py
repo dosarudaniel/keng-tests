e33ecd2640d2e539c053707b9b7e265e92dc7186
Daniel-Florin Dosaru <dosarudaniel@gmail.com>
2023-06-28 11:32:13 +0300

    Update common.py

--- py/utils/common.py
+++ py/utils/common.py
@@ -705,15 +705,15 @@ def print_stats(port_stats=None, flow_stats=None, clear_screen=None):
         print(border)
         print(
             row_format.format(
-                'Port', 'Tx Frames', 'Tx Bytes', 'Rx Frames', 'Rx Bytes',
-                'Tx FPS', 'Tx Bytes Rate', 'Rx FPS', 'Rx Bytes Rate'
+                'Port', 'Tx Frames', 'Tx Bytes', 'Tx FPS', 'Rx Frames', 'Rx Bytes',
+                'Tx Bytes Rate', 'Rx FPS', 'Rx Bytes Rate'
             )
         )
         for stat in port_stats:
             print(
                 row_format.format(
-                    stat.name, stat.frames_tx, stat.bytes_tx, stat.frames_rx,
-                    stat.bytes_rx, stat.frames_tx_rate, stat.bytes_tx_rate,
+                    stat.name, stat.frames_tx, stat.bytes_tx, stat.frames_tx_rate, stat.frames_rx,
+                    stat.bytes_rx, stat.bytes_tx_rate,
                     stat.frames_rx_rate, stat.bytes_rx_rate
                 )
             )
