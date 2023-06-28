94727f9baf7527cafa8743bc99f7fdd014c9d2b0
Daniel-Florin Dosaru <dosarudaniel@gmail.com>
2023-06-28 12:06:19 +0300

    Update rfc2544_test.sh

--- rfc2544_test.sh
+++ rfc2544_test.sh
@@ -5,5 +5,7 @@ cd /home/ixia/ixia-c-tests
 python3 -m pytest ./py/test_throughput_rfc2544.py
 
 cat throughput_results.json  | jq
-cat throughput_results.json  | jq > throughput_results.json
+cat throughput_results.json  | jq > tmp.json
+cat tmp.json > throughput_results.json
+rm tmp.json
 
