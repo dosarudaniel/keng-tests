35538a8552d745b47cb64f2c3d98511e354a8bcb
armdupre <a-joseph.dupre@keysight.com>
2023-11-01 12:30:48 -0400

    Initial Checkin.

---
+++ cloud/ixia-c-dpdk-azure/main.tf
@@ -0,0 +1,44 @@
+module "Agent1" {
+	source = "armdupre/module-ubuntu-linux-agent/azurerm"
+	EnableAcceleratedNetworking = false
+	Eth0SubnetId = module.Vnet.PublicSubnet.id
+	Eth1SubnetId = module.Vnet.PrivateSubnet.id
+	InstanceId = local.Agent1InstanceId
+	ResourceGroupLocation = azurerm_resource_group.ResourceGroup.location
+	ResourceGroupName = azurerm_resource_group.ResourceGroup.name
+	SshKeyName = azurerm_ssh_public_key.SshKey.name
+	UserEmailTag = local.UserEmailTag
+	UserLoginTag = local.UserLoginTag
+	VmSize = local.AgentVmSize
+	init_cli = data.cloudinit_config.init_cli.rendered
+	depends_on = [
+		azurerm_ssh_public_key.SshKey,
+		module.Vnet
+	]
+}
+
+module "Agent2" {
+	source = "armdupre/module-ubuntu-linux-agent/azurerm"
+	EnableAcceleratedNetworking = false
+	Eth0IpAddress = local.Agent2Eth0IpAddress
+	Eth0SubnetId = module.Vnet.PublicSubnet.id
+	Eth1IpAddresses = local.Agent2Eth1IpAddresses
+	Eth1SubnetId = module.Vnet.PrivateSubnet.id
+	InstanceId = local.Agent2InstanceId
+	ResourceGroupLocation = azurerm_resource_group.ResourceGroup.location
+	ResourceGroupName = azurerm_resource_group.ResourceGroup.name
+	SshKeyName = azurerm_ssh_public_key.SshKey.name
+	UserEmailTag = local.UserEmailTag
+	UserLoginTag = local.UserLoginTag
+	VmSize = local.AgentVmSize
+	init_cli = data.cloudinit_config.init_cli.rendered
+	depends_on = [
+		azurerm_ssh_public_key.SshKey,
+		module.Vnet
+	]
+}
+
+resource "azurerm_resource_group" "ResourceGroup" {
+	name = local.ResourceGroupName
+	location = local.ResourceGroupLocation
+}
