0fdcb34b021dbc7c2598b661869b62c7d44ecbda
armdupre <a-joseph.dupre@keysight.com>
2023-12-07 15:11:04 -0500

    Updated.

--- cloud/ixia-c-dpdk-azure/deployment/setup2.sh
+++ cloud/ixia-c-dpdk-azure/deployment/setup2.sh
@@ -8,19 +8,10 @@ PRIMARY="eth1"
 # > enP30832p0s0     UP             f0:0d:3a:ec:b4:0a <... # truncated
 # grab interface name for device bound to primary
 SECONDARY="`ip -br link show master $PRIMARY | awk '{ print $1 }'`"
-# Get mac address for MANA interface (should match primary)
-MANA_MAC="`ip -br link show master $PRIMARY | awk '{ print $3 }'`"
-
-
-# $ ethtool -i enP30832p0s0 | grep bus-info
-# > bus-info: 7870:00:00.0
-# get MANA device bus info to pass to DPDK
-BUS_INFO="`ethtool -i $SECONDARY | grep bus-info | awk '{ print $2 }'`"
 
 # Set MANA interfaces DOWN before starting DPDK
 ip link set $PRIMARY down
 ip link set $SECONDARY down
-
 
 ## Move synthetic channel to user mode and allow it to be used by NETVSC PMD in DPDK
 DEV_UUID=$(basename $(readlink /sys/class/net/$PRIMARY/device))
@@ -29,9 +20,3 @@ modprobe uio_hv_generic
 echo $NET_UUID > /sys/bus/vmbus/drivers/uio_hv_generic/new_id
 echo $DEV_UUID > /sys/bus/vmbus/drivers/hv_netvsc/unbind
 echo $DEV_UUID > /sys/bus/vmbus/drivers/uio_hv_generic/bind
-
-# MANA single queue test
-#dpdk-testpmd -l 1-3 --vdev="$BUS_INFO,mac=$MANA_MAC" -- --forward-mode=txonly --auto-start --txd=128 --rxd=128 --stats 2
-
-# MANA multiple queue test (example assumes > 9 cores)
-#dpdk-testpmd -l 1-9 --vdev="$BUS_INFO,mac=$MANA_MAC" -- --forward-mode=txonly --auto-start --nb-cores=8  --txd=128 --rxd=128 --txq=8 --rxq=8 --stats 2
