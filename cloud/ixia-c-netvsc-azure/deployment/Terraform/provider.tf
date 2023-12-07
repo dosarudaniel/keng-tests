4046def4229b4220a1a3f9c87e66d49b9b6d2b78
armdupre <a-joseph.dupre@keysight.com>
2023-12-07 14:30:56 -0500

    Duplicate.

--- cloud/ixia-c-netvsc-azure/deployment/Terraform/provider.tf
+++
@@ -1,8 +0,0 @@
-provider "azurerm" {
-	subscription_id = var.SubscriptionId
-	features {
-		resource_group {
-			prevent_deletion_if_contains_resources = false
-		}
-	}
-}
--- cloud/ixia-c-netvsc-azure/deployment/Terraform/terraform.optional.auto.tfvars
+++
@@ -1,3 +0,0 @@
-# AgentVmSize = "Standard_F8s_v2"
-# ResourceGroupLocation = "East US"
-# UserProjectTag = "cloud-ist"
