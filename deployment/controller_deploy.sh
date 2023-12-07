71ab823eb2fc762eba60d5448822223ccf1259f4
armdupre <a-joseph.dupre@keysight.com>
2023-12-07 16:41:23 -0500

    Duplicate.

--- deployment/controller_deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Remove an old docker container with the same name if existing
-docker rm Ixia-c-Controller
-
-# Deploy the KENG controller
-sudo docker run -d --name Ixia-c-Controller --network=host \
-ghcr.io/open-traffic-generator/licensed/ixia-c-controller:latest --accept-eula
