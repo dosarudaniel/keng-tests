39da312c2d0bf4d34dc59b50f49a0ab8e481e1bb
armdupre <a-joseph.dupre@keysight.com>
2023-12-07 16:45:33 -0500

    Initial Checkin.

---
+++ cloud/ixia-c-dpdk-azure/deployment/Terraform/locals.tf
@@ -0,0 +1,36 @@
+locals {
+	AgentVmSize = var.AgentVmSize
+	Agent1InstanceId = "agent1"
+	Agent1Eth1IpAddresses = [ "10.0.2.12", "10.0.2.32", "10.0.2.52" ]
+	Agent1Eth2IpAddresses = [ "10.0.2.112", "10.0.2.132", "10.0.2.152" ]
+	Agent2Eth0IpAddress = "10.0.10.12"
+	Agent2Eth1IpAddresses = [ "10.0.2.22", "10.0.2.42", "10.0.2.62" ]
+	Agent2Eth2IpAddresses = [ "10.0.2.122", "10.0.2.142", "10.0.2.162" ]
+	Agent2InstanceId = "agent2"
+	AppTag = "ubuntu"
+	AppVersion = "2204-lts"
+	ClientId = var.ClientId
+	ClientSecret = var.ClientSecret
+	DockerComposeServices = yamldecode(file("../docker-compose.yaml"))["services"]
+	GitRepoConfigPath = "${local.GitRepoExecPath}/configs"
+	GitRepoExecPath = "cloud/ixia-c-dpdk-azure"
+	GitRepoDeployPath = "${local.GitRepoExecPath}/deployment/"
+	GitRepoName = var.GitRepoName
+	GitRepoUrl = var.GitRepoUrl
+	KengControllerImage = local.KengControllerService["image"]
+	KengControllerService = local.DockerComposeServices["controller"]
+	KengTrafficEngineImage = local.KengTrafficEngineService["image"]
+	KengTrafficEngineService = local.DockerComposeServices["TE1-5551"]
+	Preamble = "${local.UserLoginTag}-${local.AppTag}-${local.AppVersion}"
+	PublicSecurityRuleSourceIpPrefixes = var.PublicSecurityRuleSourceIpPrefixes
+	ResourceGroupLocation = var.ResourceGroupLocation
+	ResourceGroupName = var.ResourceGroupName
+	SshKeyAlgorithm = "RSA"
+	SshKeyName = "${local.Preamble}-ssh-key"
+	SshKeyRsaBits = "4096"
+	SubscriptionId = var.SubscriptionId
+	TenantId = var.TenantId
+	UserEmailTag = var.UserEmailTag
+	UserLoginTag = var.UserLoginTag
+	UserProjectTag = var.UserProjectTag
+}
---
+++ cloud/ixia-c-dpdk-azure/deployment/Terraform/terraform.required.auto.tfvars
@@ -0,0 +1,9 @@
+# ClientId = "00000000-0000-0000-0000-000000000000"
+# ClientSecret = "ABCDEFGHIJKLMNOPQRSTUVWXYZ01234567"
+# GitRepoUrl = "-b azure https://github.com/dosarudaniel/ixia-c-tests.git"
+# PublicSecurityRuleSourceIpPrefixes = [ "1.1.1.1/32" ]
+# ResourceGroupName = "azure-ubuntu-linux-2-agents"
+# SubscriptionId = "00000000-0000-0000-0000-000000000000"
+# TenantId = "00000000-0000-0000-0000-000000000000"
+# UserEmailTag = "terraform@example.com"
+# UserLoginTag = "terraform"
