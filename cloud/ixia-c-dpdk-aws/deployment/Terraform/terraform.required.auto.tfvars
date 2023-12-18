d6e48d32a92b9e7cfe3f81515dadb330e21725b3
armdupre <a-joseph.dupre@keysight.com>
2023-12-18 14:34:29 -0500

    Initial Checkin.

---
+++ cloud/ixia-c-dpdk-aws/deployment/Terraform/terraform.required.auto.tfvars
@@ -0,0 +1,4 @@
+# AwsAccessCredentialsAccessKey = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+# AwsAccessCredentialsSecretKey = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+# InboundIPv4CidrBlocks = [ "1.1.1.1/32" ]
+# UserEmailTag = "terraform@example.com"
---
+++ cloud/ixia-c-dpdk-aws/deployment/Terraform/variables.tf
@@ -0,0 +1,91 @@
+variable "AgentInstanceType" {
+	default = "c5.4xlarge"
+	description = "Instance type of Agent VM"
+	type = string
+	validation {
+		condition = contains([	"c5.xlarge", "c5.2xlarge", "c5.4xlarge",
+								"c4.8xlarge"
+							], var.AgentInstanceType)
+		error_message = <<EOF
+AgentInstanceType must be one of the following types:
+	c5.xlarge, c5.2xlarge, c5.4xlarge,
+	c4.8xlarge
+		EOF
+	}
+}
+
+variable "ApiMaxRetries" {
+	default = 1
+	type = number
+}
+
+variable "AwsAccessCredentialsAccessKey" {
+	description = "Access key component of credentials used for programmatic calls to AWS."
+	type = string
+}
+
+variable "AwsAccessCredentialsSecretKey" {
+	description = "Secret access key component of credentials used for programmatic calls to AWS."
+	type = string
+}
+
+variable "AwsMetadataServerUrl" {
+	default = "http://169.254.169.254/latest/meta-data"
+	type = string
+}
+
+variable "GitRepoName" {
+	default = "keng-python"
+	type = string
+}
+
+variable "GitRepoUrl" {
+	default = "-b cloud https://github.com/open-traffic-generator/otg-examples.git"
+	sensitive = true
+	type = string
+}
+
+variable "InboundIPv4CidrBlocks" {
+	description = "List of IP Addresses /32 or IP CIDR ranges connecting inbound to App"
+	type = list(string)
+}
+
+variable "PrivateSubnetAvailabilityZone" {
+	default = "us-east-1a"
+	type = string
+}
+
+variable "PublicSubnetAvailabilityZone" {
+	default = "us-east-1a"
+	type = string
+}
+
+variable "Region" {
+	default = "us-east-1"
+	type = string
+}
+
+variable "UserEmailTag" {
+	description = "Email address tag of user creating the deployment"
+	type = string
+	validation {
+		condition = length(var.UserEmailTag) >= 14
+		error_message = "UserEmailTag minimum length must be >= 14."
+	}
+}
+
+variable "UserLoginTag" {
+	default = "terraform"
+	description = "Login ID tag of user creating the deployment"
+	type = string
+	validation {
+		condition = length(var.UserLoginTag) >= 4
+		error_message = "UserLoginTag minimum length must be >= 4."
+	}
+}
+
+variable "UserProjectTag" {
+	default = "cloud-ist"
+	description = "Project tag of user creating the deployment"
+	type = string
+}
