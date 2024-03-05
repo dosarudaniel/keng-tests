8040109c152910ebeef4fd277d57f58948014ad9
armdupre <a-joseph.dupre@keysight.com>
2024-03-05 16:14:04 -0500

    Updated.

--- cloud/ixia-c-dpdk-azure-boost-eth2/deployment/setup.sh
+++ cloud/ixia-c-dpdk-azure-boost-eth2/deployment/setup.sh
@@ -4,9 +4,6 @@ echo 2048 | tee /sys/devices/system/node/node*/hugepages/hugepages-2048kB/nr_hug
 # Assuming use of eth1 for DPDK in this demo
 PRIMARY="eth1"
 
-# $ ip -br link show master eth1 
-# > enP30832p0s0     UP             f0:0d:3a:ec:b4:0a <... # truncated
-# grab interface name for device bound to primary
 SECONDARY="`ip -br link show master $PRIMARY | awk '{ print $1 }'`"
 
 # Set MANA interfaces DOWN before starting DPDK
@@ -26,9 +23,6 @@ echo $DEV_UUID > /sys/bus/vmbus/drivers/uio_hv_generic/bind
 # Assuming use of eth2 for DPDK in this demo
 PRIMARY="eth2"
 
-# $ ip -br link show master eth2 
-# > enP30832p0s0     UP             f0:0d:3a:ec:b4:0a <... # truncated
-# grab interface name for device bound to primary
 SECONDARY="`ip -br link show master $PRIMARY | awk '{ print $1 }'`"
 
 # Set MANA interfaces DOWN before starting DPDK
@@ -37,8 +31,8 @@ ip link set $SECONDARY down
 
 ## Move synthetic channel to user mode and allow it to be used by NETVSC PMD in DPDK
 DEV_UUID=$(basename $(readlink /sys/class/net/$PRIMARY/device))
-NET_UUID="f7615163-df3e-46c5-913f-f2d2f965ed0e"
+# NET_UUID="f7615163-df3e-46c5-913f-f2d2f965ed0e"
 modprobe uio_hv_generic
-echo $NET_UUID > /sys/bus/vmbus/drivers/uio_hv_generic/new_id
+# echo $NET_UUID > /sys/bus/vmbus/drivers/uio_hv_generic/new_id
 echo $DEV_UUID > /sys/bus/vmbus/drivers/hv_netvsc/unbind
 echo $DEV_UUID > /sys/bus/vmbus/drivers/uio_hv_generic/bind
